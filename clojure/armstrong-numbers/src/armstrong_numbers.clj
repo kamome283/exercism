054dae0f1578c9582d9bcfce8e0e6cc76eb3a7a7
AsoberNet <26428614+AsoberNet@users.noreply.github.com>
2021-04-04 14:21:18 +0900

    Fix/Clojure/Armstrong Numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,9 +1,17 @@
 (ns armstrong-numbers)
 
+(defn- digits [num]
+  (->> num
+       (iterate #(quot % 10))
+       (take-while pos?)
+       (map #(mod % 10))))
+
+(defn- pow [base exp]
+  (->> base (repeat exp) (apply *)))
+
 (defn armstrong? [num]
-  (let [str-num (str num)
-        len (count str-num)]
-    (->> str-num
-         (map #(-> % str BigInteger. (.pow len)))
+  (let [ds (digits num)]
+    (->> ds
+         (map #(pow % (count ds)))
          (apply +)
          (= num))))
