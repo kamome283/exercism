8d5f383666ad0b26e2205a1cd0a2db9404a204e4
AsoberNet <26428614+AsoberNet@users.noreply.github.com>
2021-04-09 16:42:02 +0900

    Fix/Clojure/Run Length Encoding

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,19 +1,21 @@
 (ns run-length-encoding)
 
-(defn- encode-step [s]
-  (let [ch (first s)
-        cnt (count s)]
-    (if (= cnt 1)
-      ch
-      (str cnt ch))))
+(defn- encode-step [[fst & rst :as part]]
+  (cond->> fst
+           (not-empty rst) (str (count part))))
 
 (defn run-length-encode [text]
-  (->> text (partition-by identity) (map encode-step) (apply str)))
+  (->> text
+       (partition-by identity)
+       (map encode-step)
+       (apply str)))
 
 (defn- decode-step [[_ count-s ch]]
-  (if (empty? count-s)
-    ch
-    (->> ch (repeat (Integer/parseInt count-s)) (apply str))))
+  (cond->> ch
+           (not-empty count-s) (repeat (Integer/parseInt count-s))))
 
 (defn run-length-decode [text]
-  (->> text (re-seq #"(\d*)(\D)") (map decode-step) (apply str)))
+  (->> text
+       (re-seq #"(\d*)(\D)")
+       (mapcat decode-step)
+       (apply str)))
