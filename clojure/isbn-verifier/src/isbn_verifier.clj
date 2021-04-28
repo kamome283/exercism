62083639c43be7e2a27a361e5a5d77650a9c0b40
AsoberNet <26428614+AsoberNet@users.noreply.github.com>
2021-04-28 10:45:06 +0900

    Fix/Clojure/ISBN Verifier

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,13 +1,16 @@
 (ns isbn-verifier)
 
+(defn- to-num [c]
+  (if (= c \X)
+    10
+    (Character/digit c 10)))
+
 (defn isbn? [isbn]
-  (or (some->> (clojure.string/replace isbn #"-" "")
-               (re-matches #"\d{9}[\dX]")
-               (map #(if (= % \X)
-                       10
-                       (Character/digit % 10)))
-               (map * (range 10 0 -1))
-               (apply +)
-               (#(mod % 11))
-               zero?)
-      false))
+  (let [ds (re-matches #"\d{9}[\dX]" (clojure.string/replace isbn #"-" ""))]
+    (and (some? ds)
+         (->> ds
+              (map to-num)
+              (map * (range 10 0 -1))
+              (apply +)
+              (#(mod % 11))
+              zero?))))
